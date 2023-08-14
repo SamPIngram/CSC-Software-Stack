e023f02e589294674b44b7cef8abc421bbb4d0b6
Sam Ingram <31179948+SamPIngram@users.noreply.github.com>
2023-08-14 12:50:23 +0000

    startup script added

---
+++ docker/workstation-base/setup.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+/miniconda/bin/conda init
+code --install-extension ms-python.python
+
+
+echo -e "\n\n*****************************************"
+read -p "Setup Script Completed. Press enter to exit."
