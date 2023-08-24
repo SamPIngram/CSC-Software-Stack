d4d27b4972492bb1153607b8485263e1c3b6d32e
Sam Ingram <31179948+SamPIngram@users.noreply.github.com>
2023-08-24 16:28:15 +0000

    update to ssl conflicts on clinical networks

--- docker/workstation-base/setup/setup/setup.sh
+++ docker/workstation-base/setup/setup/setup.sh
@@ -5,9 +5,9 @@ dconf write /org/mate/desktop/interface/gtk-theme "'Yaru-dark'"
 dconf write /org/mate/desktop/background/picture-filename "'/setup/wallpaper.jpg'"
 
 # Desktop Icons
-cp /usr/share/applications/mate-terminal.desktop /config/Desktop
-cp /usr/share/applications/firefox.desktop /config/Desktop
-cp /usr/share/applications/code.desktop /config/Desktop
+cp /usr/share/applications/mate-terminal.desktop /config/Desktop/
+cp /usr/share/applications/firefox.desktop /config/Desktop/
+cp /usr/share/applications/code.desktop /config/Desktop/
 
 # Conda
 /miniconda/bin/conda update -y conda
@@ -20,6 +20,7 @@ conda install ipykernel --update-deps --force-reinstall -y
 echo "conda activate developer" >> ~/.bashrc
 
 # VS Code
+cp /setup/settings.json ~/.config/Code/User/settings.json
 code --install-extension ms-python.python
 code --install-extension ms-azuretools.vscode-docker
 code --install-extension ms-toolsai.jupyter
@@ -28,7 +29,6 @@ code --install-extension wayou.vscode-todo-highlight
 code --install-extension vscode-icons-team.vscode-icons
 code --install-extension grapecity.gc-excelviewer
 code --install-extension oderwat.indent-rainbow
-cp /setup/settings.json ~/.config/Code/User/settings.json
 
 echo -e "\n\n*****************************************"
 read -p "Setup Script Completed. Press enter to exit."
