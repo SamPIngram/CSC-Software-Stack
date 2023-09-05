9a25a455bb87c5acd60702111df9d67270047dd8
Sam Ingram <31179948+SamPIngram@users.noreply.github.com>
2023-09-05 11:03:39 +0000

    Start work on report scripting

---
+++ scripts/run_report.sh
@@ -0,0 +1,46 @@
+#!/usr/bin/env bash
+
+# Run this script to generate cves and sbom reports
+
+# Parse cmd arguments
+
+CONTEXT=""
+SRC_BRANCH="master"
+DEPLOY_BRANCH="gh-pages"
+
+USAGE_MSG="usage: deploy [-h|--help] [-c|--context CONTEXT] [-s|--src SRC_BRANCH] [-d|--deploy DEPLOY_BRANCH] [--verbose] [--no-push]"
+
+while [[ $# > 0 ]]; do
+    key="$1"
+
+    case $key in
+        -h|--help)
+        echo $USAGE_MSG
+        exit 0
+        ;;
+        -u|--user)
+        SRC_BRANCH="source"
+        DEPLOY_BRANCH="master"
+        ;;
+        -s|--src)
+        SRC_BRANCH="$2"
+        shift
+        ;;
+        -d|--deploy)
+        DEPLOY_BRANCH="$2"
+        shift
+        ;;
+        --verbose)
+        set -x
+        ;;
+        --no-push)
+        NO_PUSH="--no-push"
+        ;;
+        *)
+        echo "Option $1 is unknown." >&2
+        echo $USAGE_MSG >&2
+        exit 1
+        ;;
+    esac
+    shift
+done
