e45ca0ee3050794220d525aa49aef54598eb3d7d
Sam Ingram <31179948+SamPIngram@users.noreply.github.com>
2023-09-14 16:02:15 +0100

    add get_extension_manually to workstation if needed

--- docker/workstation-base/setup/scripts/get_extensions_manually.sh
+++ docker/workstation-base/setup/scripts/get_extensions_manually.sh
@@ -2,8 +2,8 @@
 
 # Only use if vscode.sh is not able to download extensions due to ssl issues
 
-cd /config/
-rm -r /config/extensions
+cd /config/.vscode
+rm -r /config/.vscode/extensions
 wget https://github.com/SamPIngram/CSC-Software-Stack/releases/download/v0.12/extensions.zip --no-check-certificate
 unzip extensions.zip
 rm extensions.zip
