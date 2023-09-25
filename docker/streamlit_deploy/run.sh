c6968787d7fc9dc78c3361f0199f7a0d1c358bbc
Sam Ingram <31179948+SamPIngram@users.noreply.github.com>
2023-09-25 15:53:05 +0000

    Example setup

--- docker/streamlit_deploy/run.sh
+++ docker/streamlit_deploy/run.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
-git -c http.sslVerify=false clone $GITHUB .
+rm -r /app
+
+mkdir /app
+
+cd /app
+
+git -c http.sslVerify=false clone $REPO .
 
 pip3 install -r requirements.txt --trusted-host pypi.python.org --trusted-host files.pythonhosted.org --trusted-host pypi.org
 
