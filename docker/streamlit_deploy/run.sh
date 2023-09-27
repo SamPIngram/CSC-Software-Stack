47691b46cf727b78722733a29f852ffde744d242
Sam Ingram <31179948+SamPIngram@users.noreply.github.com>
2023-09-27 15:28:28 +0000

    update

--- docker/streamlit_deploy/run.sh
+++ docker/streamlit_deploy/run.sh
@@ -23,5 +23,15 @@ elif [ -z "${DIR}" ]; then
 elif [ -z "${REPO}" ]; then
     echo "No Github given, attempting to use directory."
     cd $REPO
-    .conda/envs/developer/bin/python -m streamlit run $ENTRY --server.port=8501 --server.address=0.0.0.0
+    /miniconda/bin/conda config --set ssl_verify False
+    /miniconda/bin/conda update -y conda
+    /miniconda/bin/conda init
+    source ~/.bashrc
+    if [ -f "environment.yml" ]; then
+        conda install -c conda-forge mamba -y
+        mamba env create -f environment.yml --prefix ~/.conda/envs/developer --force 
+        ~/.conda/envs/developer/bin/python streamlit run $ENTRY --server.port=8501 --server.address=0.0.0.0
+    elif [ -f "requirements.txt" ]; then
+        /miniconda/bin/python -m pip install -r requirements.txt --trusted-host pypi.python.org --trusted-host files.pythonhosted.org --trusted-host pypi.org
+        /miniconda/bin/python -m streamlit run $ENTRY --server.port=8501 --server.address=0.0.0.0
 fi
