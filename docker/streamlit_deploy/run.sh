dc9fb634db82f07015aaa55c49a5b4b3904f5bb8
Sam Ingram <31179948+SamPIngram@users.noreply.github.com>
2023-09-27 16:55:11 +0100

    Update run.sh

--- docker/streamlit_deploy/run.sh
+++ docker/streamlit_deploy/run.sh
@@ -34,6 +34,7 @@ elif [ -z "${REPO}" ]; then
     elif [ -f "requirements.txt" ]; then
         /miniconda/bin/python -m pip install -r requirements.txt --trusted-host pypi.python.org --trusted-host files.pythonhosted.org --trusted-host pypi.org
         /miniconda/bin/python -m streamlit run $ENTRY --server.port=8501 --server.address=0.0.0.0
+    fi
 else
     echo "You've given both a DIR and REPO. One should be left blank!"
 fi
