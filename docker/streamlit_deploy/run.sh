0d1877507d83a495843805a4b2734587b3a82ff7
Sam Ingram <31179948+SamPIngram@users.noreply.github.com>
2025-08-06 16:12:23 +0100

    Update run.sh

--- docker/streamlit_deploy/run.sh
+++ docker/streamlit_deploy/run.sh
@@ -13,9 +13,9 @@ elif [ -z "${DIR}" ]; then
     /miniconda/bin/conda init
     source ~/.bashrc
     if [ -f "environment.yml" ]; then
-        conda install -c conda-forge mamba -y
-        mamba env create -f environment.yml --prefix ~/.conda/envs/developer
-        ~/.conda/envs/developer/bin/python streamlit run $ENTRY --server.port=8501 --server.address=0.0.0.0
+       rm -rf ~/.conda/envs/developer
+        /miniconda/bin/conda env create -f environment.yml --prefix ~/.conda/envs/developer 
+        ~/.conda/envs/developer/bin/python -m streamlit run $ENTRY --server.port=8501 --server.address=0.0.0.0
     elif [ -f "requirements.txt" ]; then
         /miniconda/bin/python -m pip install -r requirements.txt --trusted-host pypi.python.org --trusted-host files.pythonhosted.org --trusted-host pypi.org
         /miniconda/bin/python -m streamlit run $ENTRY --server.port=8501 --server.address=0.0.0.0
@@ -33,10 +33,9 @@ elif [ -z "${REPO}" ]; then
     /miniconda/bin/conda init
     source ~/.bashrc
     if [ -f "environment.yml" ]; then
-        conda install -c conda-forge mamba -y
-        rm ~/.conda/envs/developer
-        mamba env create -f environment.yml --prefix ~/.conda/envs/developer 
-        ~/.conda/envs/developer/bin/python streamlit run $ENTRY --server.port=8501 --server.address=0.0.0.0
+        rm -rf ~/.conda/envs/developer
+        /miniconda/bin/conda env create -f environment.yml --prefix ~/.conda/envs/developer 
+        ~/.conda/envs/developer/bin/python -m streamlit run $ENTRY --server.port=8501 --server.address=0.0.0.0
     elif [ -f "requirements.txt" ]; then
         /miniconda/bin/python -m pip install -r requirements.txt --trusted-host pypi.python.org --trusted-host files.pythonhosted.org --trusted-host pypi.org
         /miniconda/bin/python -m streamlit run $ENTRY --server.port=8501 --server.address=0.0.0.0
